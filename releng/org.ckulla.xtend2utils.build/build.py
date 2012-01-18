d71c7b057be89cd5051f16fe4b64deee13d9518c
Christoph Kulla <christophkulla@googlemail.com>
2012-01-18 20:53:12 +0100

    Added a build script which creates the local repository and runs maven

---
+++ releng/org.ckulla.xtend2utils.build/build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 
+
+import os
+import subprocess
+import sys
+import string
+
+def fileIsNewerThan(file, dependentFile):
+	return (not os.path.exists (dependentFile)) or os.path.getmtime(file)>os.path.getmtime(dependentFile)
+
+def createLocalRepositoryIfNotUpToDate ():
+	if (fileIsNewerThan ('../../releng/org.ckulla.xtend2utils.repository/category.xml', 
+	                    '../../releng/org.ckulla.xtend2utils.repository/target/category.xml') or
+	   fileIsNewerThan ('../../releng/org.ckulla.xtend2utils.targetplatform/xtend2utils.target', 
+					    '../../releng/org.ckulla.xtend2utils.repository/target/category.xml')): 
+		print 'Rebuild of repository required'
+		subprocess.call ("cd ../../releng/org.ckulla.xtend2utils.repository && mvn package", shell=True)
+	else:
+		print 'Repository is up to date'
+
+def runMaven():
+	s = 'mvn ' + string.join (sys.argv[1:], ' ') + ' -Dlocal-build'
+	print "Calling " + '"' + s + "'"
+	subprocess.call (s, shell=True)
+	
+createLocalRepositoryIfNotUpToDate()
+runMaven()

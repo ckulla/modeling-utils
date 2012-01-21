6da8e72a401c2c6bea3d622f2414a746169e1955
Christoph Kulla <christophkulla@googlemail.com>
2012-01-21 13:08:37 +0100

    Release script supports now a dry-run (no changes will be committed)

--- releng/org.ckulla.xtend2utils.parent/release.py
+++ releng/org.ckulla.xtend2utils.parent/release.py
@@ -4,10 +4,8 @@ import os
 import subprocess
 import sys
 import string
-
-projectName = "org.ckulla.xtend2utils"
-releaseDir = projectName + ".releases"
-updatesiteDir = projectName + ".updatesite"
+import argparse
+import subprocess
 
 def echo (s):
 	print ("[### Release ###] " + s)
@@ -15,34 +13,66 @@ def echo (s):
 def call (s):
 	echo (s)
 	subprocess.check_call(s, shell=True)
-	
-if sys.argv.count >= 3:
 
-	releaseVersion = sys.argv[1]
-	devVersion = sys.argv[2]
+def isGitWorikingDirectoryClean():
+	cmd = subprocess.Popen('git status --porcelain', shell=True, stdout=subprocess.PIPE)
+	for line in cmd.stdout:
+		return False
+	return True
+	
+def revertChanges():
+	call ("git clean -f")
+	
+projectName = "org.ckulla.xtend2utils"
+releaseDir = projectName + ".releases"
+updatesiteDir = projectName + ".updatesite"
+
+# Parse command line options
+
+parser = argparse.ArgumentParser(description='Run a release build for "' + projectName +'"')
+parser.add_argument('releaseVersion', metavar='RV', help='the version number for the release, e.g. 1.0.0')
+parser.add_argument('devVersion', metavar='DV', help='the version number for the next development cycle, e.g. 2.0.0. Do not add SNAPSHOT to it, this will be done automatically')
+parser.add_argument('--dry-run', dest='dryRun', action='store_true',
+                   help='do a dry run (do not checkin changes into git, revert all changes after the release script is done). Do this to verify the release will be successfull beforing doing it.')
+args = parser.parse_args()
+
+echo ("Release version: " + args.releaseVersion + ", development version: " + args.devVersion + "-SNAPSHOT")
+if args.dryRun:
+	echo ("Dry Run, no changes will be committed")
+
+if isGitWorikingDirectoryClean():
 
 	# Doing the release
-
-	echo ("Set new version to release version " + releaseVersion)
-	call ("mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=" + releaseVersion)
+	
+	echo ("Set new version to release version " + args.releaseVersion)
+	call ("mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=" + args.releaseVersion)
 	call ("sed -i .bak 's/.qualifier//g' ../" + updatesiteDir + "/category.xml")
 	call ("./build.py clean verify")
-	call ("mv ../" + updatesiteDir + "/target/" + updatesiteDir + ".zip ../" + releaseDir + "/" + releaseDir + "-" + releaseVersion + ".zip")
-	call ("git commit -a -m '[release]	Release " + releaseVersion + "'")
-	call ("git tag v" + releaseVersion)
-
+	call ("mv ../" + updatesiteDir + "/target/" + updatesiteDir + ".zip ../" + releaseDir + "/" + releaseDir + "-" + args.releaseVersion + ".zip")
+	
+	if not args.dryRun:
+		call ("git commit -a -m '[release]	Release " + args.releaseVersion + "'")
+		call ("git tag v" + args.releaseVersion)
+	
 	# Setting the development version
-
-	echo ("Set new version to development version " + devVersion + "-SNAPSHOT")
-	call ("mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=" + devVersion + "-SNAPSHOT")
-	call ('sed -i .bak "s/' + releaseVersion + '/' + devVersion + '\.qualifier/g" ../' + updatesiteDir + '/category.xml')
+	
+	echo ("Set new version to development version " + args.devVersion + "-SNAPSHOT")
+	call ("mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=" + args.devVersion + "-SNAPSHOT")
+	call ('sed -i .bak "s/' + args.releaseVersion + '/' + args.devVersion + '\.qualifier/g" ../' + updatesiteDir + '/category.xml')
 	call ("./build.py clean verify")
-	call ("git commit -a -m '[release] Set version to " + devVersion + "-SNAPSHOT'")
+	
+	if not args.dryRun:
+		call ("git commit -a -m '[release] Set version to " + args.devVersion + "-SNAPSHOT'")
+	
+	# Success message
+	
+	echo ("Release " + args.releaseVersion + " successfully created")
+	echo ("Current version is now " + args.devVersion + "-SNAPSHOT")
 
-	print ("Release " + releaseVersion + " successfully created")
-	print ("Current development version is now " + devVersion + "-SNAPSHOT")
-	
+	if args.dryRun:
+		revertChanges
+	if isGitWorikingDirectoryClean:
+		echo ("Your working directory is clean")
 else:
-	
-	printf ("Usage: release.py RELEASE_VERSION DEVELOPMENT_VERSION")
-	printf ("e.g. release.py 0.2 0.3-SNAPSHOT")
+
+	echo ("Your working directory is not clean, aborting...")
